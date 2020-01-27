bcda2e192a3920c7015e74affc77c256f81e57ca
Edgar Muniz Berlinck <edgar.vv@gmail.com>
2020-01-27 18:17:49 -0300

    #1 Crawler for technadu

---
+++ content_generator/content_generator/spiders/technadu_spider.py
@@ -0,0 +1,21 @@
+import scrapy
+
+
+class QuotesSpider(scrapy.Spider):
+    name = "technadu"
+
+    def start_requests(self):
+        urls = [
+            'https://www.technadu.com/news/',
+        ]
+        for url in urls:
+            yield scrapy.Request(url=url, callback=self.parse)
+
+    def parse(self, response):
+        nodes = response.css('.item-details > .entry-title > a')
+        for node in nodes:
+            full_url = node.attrib['href']
+            title = node.attrib['title']
+            page_id = full_url.split('/')[-2]
+            # TODO now I have to save-it
+            
